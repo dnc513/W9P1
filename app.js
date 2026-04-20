100b90388c3093bafaf7e7f035e9fbe98a9e167b
dnc513 <dnc513@york.ac.uk>
2026-04-20 16:56:52 +0100

    04.20

---
+++ app.js
@@ -0,0 +1,52 @@
+const DB_ENDPOINT = "https://script.google.com/macros/s/AKfycbzI4zLoiTpULUlr5lBeykqBZuJYejBeIGgnL4aH8oD9-https://script.google.com/macros/s/AKfycbzI4zLoiTpULUlr5lBeykqBZuJYejBeIGgnL4aH8oD9-JGEjTaVN8XQqdiVRyrCzPQWkQ/exec/exec";
+ 
+//READ
+async function readData(table) {
+	try {
+        const response = await fetch(`${DB_ENDPOINT}?table=${table}`);
+        const data = await response.json();
+        console.log(data);
+	} catch (error) { console.error("Error:", error); }
+}
+ 
+//ADD
+async function addData(table, dataToAdd) {
+	try {
+        const response = await fetch(`${DB_ENDPOINT}?table=${table}`, {
+            method: "POST",
+            headers: { "Content-Type": "text/plain;charset=utf-8" },
+            body: JSON.stringify({ action: "add", data: dataToAdd })
+        });
+        const result = await response.json();
+        console.log(result);
+	} catch (error) { console.error("Error:", error); }
+}
+ 
+//UPDATE
+async function updateData(table, dataToSelect, dataToUpdate) {
+	try {
+        const response = await fetch(`${DB_ENDPOINT}?table=${table}`, {
+            method: "POST",
+            headers: { "Content-Type": "text/plain;charset=utf-8" },
+            body: JSON.stringify({ action: "update",
+                data: { select: dataToSelect, update: dataToUpdate } })
+        });
+        const result = await response.json();
+        console.log(result);
+	} catch (error) { console.error("Error:", error); }
+}
+ 
+//DELETE
+async function deleteData(table, dataToSelect) {
+	try {
+        const response = await fetch(`${DB_ENDPOINT}?table=${table}`, {
+            method: "POST",
+            headers: { "Content-Type": "text/plain;charset=utf-8" },
+            body: JSON.stringify({ action: "delete",
+                data: { select: dataToSelect } })
+        });
+        const result = await response.json();
+        console.log(result);
+	} catch (error) { console.error("Error:", error); }
+}
+
